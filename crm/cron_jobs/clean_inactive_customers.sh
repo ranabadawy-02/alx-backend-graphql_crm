b70f593b774b272d8380c155e0a511784dd1c7c1
ranabadawy-02 <ranamohsen1102@gmail.com>
2026-01-11 21:08:56 +0200

    Update clean_inactive_customers.sh

--- crm/cron_jobs/clean_inactive_customers.sh
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -2,16 +2,18 @@
 
 PROJECT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
 
-DELETED_COUNT=$(python3 $PROJECT_DIR/manage.py shell <<EOF
+count=$(python3 $PROJECT_DIR/manage.py shell <<EOF
 from datetime import timedelta
 from django.utils import timezone
 from crm.models import Customer
 
 one_year_ago = timezone.now() - timedelta(days=365)
-qs = Customer.objects.filter(orders__isnull=True) | Customer.objects.filter(orders__created_at__lt=one_year_ago)
-deleted, _ = qs.distinct().delete()
-print(deleted)
+count, _ = Customer.objects.filter(
+    orders__isnull=True
+).delete()
+
+print(count)
 EOF
 )
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Deleted customers: $DELETED_COUNT" >> /tmp/customercleanuplog.txt
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Deleted customers count: $count" >> /tmp/customercleanuplog.txt
